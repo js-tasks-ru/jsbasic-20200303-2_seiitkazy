0f567ffcd44f6f63bb45447102125dbd0749a3b7
Yedilkazy <edil@Mac-Pro-CA.local>
2020-03-05 17:09:53 +0600

    This reverts commit 973edfc049c50aa70fc0ea4e747155d7e0987a63.

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -6,14 +6,4 @@
  */
 function sum(m, n) {
 
-	var result = 0;
-
- 	for (var i = 0; i < arguments.length; i++) {
-    	result += arguments[i];
-  	}
-
-  	return result;
 }
-
-alert ( sum(1, 1) );
-alert ( sum(2, 5) );
