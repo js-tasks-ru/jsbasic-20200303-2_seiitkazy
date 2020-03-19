48540abb1b43e9bf9674454ece37ca08ad9b9ca1
yedilkazy <seiitkazy@gmail.com>
2020-03-19 12:57:48 +0600

    3-5 task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -5,4 +5,7 @@
  * @returns {number[]}
  */
 function filterRange(arr, a, b) {
+  let resultArr = [];
+  arr.map((item) => { if (item < b && item >= a) resultArr.push(item) });
+  return  resultArr;
 }
